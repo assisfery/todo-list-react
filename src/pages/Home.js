7c4da2c9eeade9efd557fbcdc3763a0337f81aa5
Assis Ferreira <ferreira.assisruny@gmail.com>
2022-02-05 18:53:04 -0100

    render todo list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
 function Home(){
+    
+    const todos = [
+        { id: 1, description: "todo sample" },
+        { id: 2, description: "todo sample 2" },
+    ];
+
     return (
         <main>
             <div className="container py-1">
                 <h3>My Tasks</h3>
+                {
+                    todos.map(
+                        (t) => 
+                            <div className="alert alert-secondary" key={t.id}>
+                                {t.description}
+                            </div>
+                    )
+                }
             </div>
         </main>
     );
